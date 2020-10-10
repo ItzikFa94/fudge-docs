826f6e15313538727b5109c9467132db4d9522ab
gitel <gitelman.igal@gmail.com>
2020-10-10 14:45:42 +0300

    Deploy website - based on 172a01839a69d2543811b784003e0c30de3604bb

---
+++ 710e82d3.2ce04b0c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{67:function(s){s.exports=JSON.parse('{"allTagsPath":"https://botechstudio.github.io/fudge-docs/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"https://botechstudio.github.io/fudge-docs/blog/tags/docusaurus"}')}}]);
---
+++ eb197d59.078230b0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{78:function(o){o.exports=JSON.parse('{"allTagsPath":"https://botechstudio.github.io/fudge-docs/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"https://botechstudio.github.io/fudge-docs/blog/tags/hello"}')}}]);

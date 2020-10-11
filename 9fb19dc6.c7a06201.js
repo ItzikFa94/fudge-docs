849afb5d1cc0a1ebc9160c2fd5d09ffa22ea3607
gitel <gitelman.igal@gmail.com>
2020-10-11 16:40:12 +0300

    Deploy website - based on 30fce9058c676c5d703b1bbbe444a427794b30cc

---
+++ 9fb19dc6.c7a06201.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{72:function(o){o.exports=JSON.parse('{"allTagsPath":"/fudge-docs/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/fudge-docs/blog/tags/hello"}')}}]);
---
+++ ed52b7a5.64559a06.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{79:function(s){s.exports=JSON.parse('{"allTagsPath":"/fudge-docs/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/fudge-docs/blog/tags/docusaurus"}')}}]);

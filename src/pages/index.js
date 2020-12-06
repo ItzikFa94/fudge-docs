996fbb7cc7fd4a47d9fb680e05031dde48a92126
gitel <gitelman.igal@gmail.com>
2020-12-06 19:26:33 +0200

    home page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Based on React Boilerplate',
+    imageUrl: 'img/react-boilerplate-logo.jpg',
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -18,8 +18,38 @@ const features = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'User Auth Using JWT',
+    imageUrl: 'img/jwt-logo.png',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Integrated Push Notifications With Socket.io',
+    imageUrl: 'img/socketio-logo.png',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Secure Connection to MongoDB',
+    imageUrl: 'img/mongodb-logo.png',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Routing Implemented using ExpressJS',
+    imageUrl: 'img/expressjs-logo.png',
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -28,8 +58,8 @@ const features = [
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Powered by NodeJS',
+    imageUrl: 'img/nodejs-logo.png',
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -39,7 +69,7 @@ const features = [
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -56,23 +86,28 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
+  const logoWhite = useBaseUrl('img/logo-white.svg');
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
+          <img style={{ width: '100px', height: '100px'}} src={logoWhite} alt="" />
           <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.customFields.tagline_note}</p>
+          <p className="hero__subtitle">{siteConfig.customFields.tagline_more}</p>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div style={{ height: '45px' }}></div>
           <div className={styles.buttons}>
             <Link
               className={clsx(
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/project-stracture')}>
-              Get Started
+              to={'https://fudge.botech.studio'}>
+              Download
             </Link>
           </div>
         </div>
